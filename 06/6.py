d254fd321c95660665686c058bffcfa600b5f614
Marius Sørensen <jobb@s0rensen.no>
2021-12-10 11:24:02 +0100

    Day 6 - :small_red_triangle_down: :gift: :small_red_triangle_down:

---
+++ 06/6.py
@@ -0,0 +1,35 @@
+import requests
+import numpy as np
+from math import ceil, floor
+
+any = np.any
+
+gifts = requests.get('https://julekalender-backend.knowit.no/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBallDIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--20b29549a475416a15aa81ff11b00da4c4103e67/pakker.txt?disposition=inline').text.splitlines()
+gifts = [(int(g.split(',')[0]), int(g.split(',')[1])) for g in gifts]
+
+grid = np.zeros((1, 20))
+
+fallen_counter = 0
+
+for gift in gifts:
+    whole_gift = slice(gift[0], gift[0] + gift[1])
+    left_half = slice(gift[0], gift[0] + ceil(gift[1] / 2))
+    right_half = slice(gift[0] + floor(gift[1] / 2), gift[0] + gift[1])
+    for r, row in enumerate(grid):
+        if any(row[whole_gift]):
+            if any(row[left_half]) and any(row[right_half]):
+                if r == 0:
+                    grid = np.insert(grid, 0, np.zeros((20)), axis=0)
+                    grid[r, whole_gift] = 1
+                else:
+                    grid[r - 1, whole_gift] = 1
+            else:
+                fallen_counter += 1
+            break
+        if r == len(grid) - 1:
+            grid[r, whole_gift] = 1
+            break
+
+
+print(grid)
+print(fallen_counter)
